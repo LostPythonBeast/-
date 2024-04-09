a0812c2297c67e3ae5a10342a5cd7022e437c1e3
LostPythonBeast <130903523+LostPythonBeast@users.noreply.github.com>
2024-04-09 10:18:24 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 #include <vector>
 
-void fibonacciSequence(int Q) {
+int fibonacciSum(int Q) {
     std::vector<int> fibonacci;
     fibonacci.push_back(0);
     fibonacci.push_back(1);
 
     int i = 1;
-    while (fibonacci[i] + fibonacci[i-1] < Q) {
-        fibonacci.push_back(fibonacci[i] + fibonacci[i-1]);
+    int sum = 1; // Инициализируем сумму чисел Фибоначчи
+    while (fibonacci[i] + fibonacci[i - 1] < Q) {
+        int nextFibonacci = fibonacci[i] + fibonacci[i - 1];
+        fibonacci.push_back(nextFibonacci);
+        sum += nextFibonacci; // Добавляем текущее число Фибоначчи к сумме
         i++;
     }
 
-    std::cout << "Последовательность чисел Фибоначчи меньше " << Q << ":" << std::endl;
-    for (int num : fibonacci) {
-        std::cout << num << " ";
-    }
+    return sum; // Возвращаем итоговую сумму
 }
 
 int main() {
     int Q;
-    std::cout << "Введите число Q: ";
+    std::cout << "vvedite Q: ";
     std::cin >> Q;
 
-    fibonacciSequence(Q);
+    int sum = fibonacciSum(Q);
+
+    std::cout << "summa chisel fibonachi menshix " << Q << ": " << sum << std::endl;
 
     return 0;
 }
